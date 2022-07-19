dc194e5bec1ed8cbae515f2291a2b09622288353
ririyeye <200610237@qq.com>
2022-07-19 15:37:24 +0800

    transfer debug

--- debug_transfer.py
+++ debug_transfer.py
@@ -3,42 +3,43 @@
 import os
 import fileopt
 
+import updatefirmware
 import paramiko
 from paramiko import SSHClient
 from scp import SCPClient
-from time import sleep
 import json
-
-# debug_filename = "test_bb_cfg"
-debug_filename = "p301d"
+from time import sleep
 
 
-def execcmd(remoteip, usr, password):
+def execcmd(remoteip, usr, password ,cmdfile , cmd):
     with SSHClient() as ssh:
         ssh.load_system_host_keys()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(remoteip, 22, usr, password)
 
-        if debug_filename == "p301d":
-            cmds = "/tmp/p301d --board_type 0 --stream_type 1 --pipeline 0 --auto_start | grep debug\n"
-        else:
-            cmds = "/tmp/" + debug_filename + " \n "
+        readflg = 0
 
         stdin, stdout, stderr = ssh.exec_command(
             "#!/bin/sh \n "
-            "chmod a+x /tmp/" + debug_filename + " \n "
+            "chmod a+x /tmp/" + cmdfile + " \n "
             "export LD_LIBRARY_PATH=/lib:/usr/lib:/local/lib:/local/usr/lib:$LD_LIBRARY_PATH \n"
-            + cmds, get_pty=True)
+            + cmd, get_pty=True)
+
+        print("exec " + cmd)
 
         while not stdout.channel.closed:
             if stdout.channel.recv_ready():
                 line = stdout.readline(1024)
                 print(line, end="")
+                readflg = 1
             else:
                 sleep(0.1)
 
+        if not readflg:
+            print(stdout.readline())
 
-if __name__ == "__main__":
+
+def transfer_file(nodename):
     js = fileopt.get_json_cfg('cfg.json')
     ftpcfg = js['ftp']
 
@@ -47,20 +48,23 @@ if __name__ == "__main__":
     ftpusr = ftpcfg['usr']
     ftppass = ftpcfg['pw']
 
-    gnd = js['gnd']
-    remoteip = ['ip']
-    upload_filename = debug_filename
+    upload_filename = js['transferdbg']['file']
 
-    boardusr = gnd['usr']
-    boardpass = gnd['pw']
+    nod = js[nodename]
+
+    remoteip = nod['ip']
+    boardusr = nod['usr']
+    boardpass = nod['pw']
 
     fileopt.ftpdownload(ftpip, ftpcwd, ftpusr, ftppass,
-                        debug_filename, debug_filename)
+                        upload_filename, upload_filename)
 
     #upload update file
     fileopt.scp_updatefile(remoteip, upload_filename, '/tmp/' +
                            upload_filename, boardusr, boardpass)
 
-    execcmd(remoteip, boardusr, boardpass)
+    cmd = '/tmp/' + upload_filename + ' ' + js['transferdbg']['cmd'][nodename]
 
-    os.system("pause")
+    execcmd(remoteip, boardusr, boardpass, upload_filename, cmd)
+
+    # updatefirmware.rebootcmd(remoteip, boardusr, boardpass)
---
+++ sky-transfer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import os
+import debug_transfer
+import json
+
+if __name__ == "__main__":
+    debug_transfer.transfer_file('sky')
+
+    os.system("pause")
