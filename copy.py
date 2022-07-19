897ea6106fe30f4d148dc730718cdbf7cfdc60b7
ririyeye <200610237@qq.com>
2022-07-19 14:49:42 +0800

    add scp test

---
+++ copy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import os
+import rtos_update
+import json
+import fileopt
+import shutil
+if __name__ == "__main__":
+    js = fileopt.get_json_cfg('cfg.json')
+    nod = js['sky']
+
+    remoteip = nod['ip']
+    boardusr = nod['usr']
+    boardpass = nod['pw']
+
+    upload_filename = js['filedbg']
+
+    shutil.copyfile(upload_filename , 'tmp/' + upload_filename)
+
+    #upload update file
+    fileopt.scp_updatefile(remoteip, upload_filename, '/tmp/' + upload_filename, boardusr, boardpass)
+    os.system("pause")
