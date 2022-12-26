0aa2159d8605c09d1e126e4edda6d85d8c5a51d7
ririyeye <200610237@qq.com>
2022-12-26 17:04:55 +0800

    add pull_img

---
+++ pull_img.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from distutils import filelist
+import os
+import updateonce
+import json
+import sys
+import fileopt
+import shutil
+
+
+if __name__ == "__main__":
+    js = fileopt.get_json_cfg('cfg.json')
+    ftpcfg = js['ftp']
+
+    ftpip = ftpcfg['ip']
+    ftpcwd = ftpcfg['workpath']
+    ftpusr = ftpcfg['usr']
+    ftppass = ftpcfg['pw']
+
+    file_list = js['pull_img']
+
+    localpath = 'sdk/'
+
+    if os.path.exists(localpath):
+        shutil.rmtree(localpath)
+
+    for file in file_list:
+        fileopt.ftpdownload(ftpip, ftpcwd, ftpusr, ftppass, file, localpath + file)
+
+    os.system("pause")
