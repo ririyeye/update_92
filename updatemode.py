9b51ab3bd48e18c19567f2d615ddea8c9660e5bf
ririyeye <200610237@qq.com>
2022-12-26 17:18:13 +0800

    update mode

--- updatemode.py
+++ updatemode.py
@@ -13,30 +13,56 @@ import fileopt
 import updatefirmware
 
 
-def change_to_update_mode(remoteip, usr, password, testmode='off' , test_program=''):
+def check_cmd_list(ssh, cmds):
+    rets = []
+    for cmd in cmds:
+        stdin, stdout, stderr = ssh.exec_command(cmd)
+        ps = stdout.readlines()
+        retcode = int(ps[0].strip('\n'))
+        rets.append(retcode)
+
+    return rets
+
+
+def check_rets(rets):
+    for ret in rets:
+        if ret == 1:
+            return True
+
+    return False
+
+
+def set_updateflg_and_reboot(ssh, remoteip, testmode):
+    print(remoteip + "normal mode , need reboot")
+    ssh.exec_command("touch /local/sirius-clean-system-flag")
+    ssh.exec_command("sync")
+    if testmode == 'on':
+        sleep(20)
+        ssh.exec_command("rm /local/sirius-clean-system-flag")
+    else:
+        print("reboot " + remoteip)
+        updatefirmware.rebootcmd_ssh(ssh)
+
+
+def change_to_update_mode(remoteip, usr, password, testmode='off', test_program=''):
     print(remoteip + " try update")
     with paramiko.SSHClient() as ssh:
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(remoteip, 22, usr, password)
 
         if test_program == '':
-            test_program="ps | grep ar_wdt | grep -v grep | wc -l"
+            test_program = []
+            test_program.append("ps | grep ar_wdt | grep -v grep | wc -l")
 
-        # 检查wdt,升级模式下没有该程序
-        stdin, stdout, stderr = ssh.exec_command(test_program)
-        ps = stdout.readlines()
-        retcode = int(ps[0].strip('\n'))
+        if not isinstance(test_program, list):
+            test_program = [test_program]
 
-        if retcode == 1:
-            print(remoteip + "normal mode , need reboot")
-            ssh.exec_command("touch /local/sirius-clean-system-flag")
-            ssh.exec_command("sync")
-            if testmode == 'on':
-                sleep(20)
-                ssh.exec_command("rm /local/sirius-clean-system-flag")
-            else:
-                print("reboot " + remoteip)
-                updatefirmware.rebootcmd_ssh(ssh)
+        rets = check_cmd_list(ssh, test_program)
+
+        retcode = check_rets(rets)
+
+        if retcode == True:
+            set_updateflg_and_reboot(ssh, remoteip, testmode)
         else:
             print(remoteip + "enter update mode")
 
@@ -60,6 +86,6 @@ if __name__ == "__main__":
 
     cmd = 'ps | grep p301d | grep -v grep | wc -l'
 
-    change_to_update_mode(ip, boardusr, boardpass, 'off' , test_program=cmd)
+    change_to_update_mode(ip, boardusr, boardpass, 'off', test_program=cmd)
 
     os.system("pause")
