de8495f81fd3131edb6b28fecca677a00c0acbba
ririyeye <200610237@qq.com>
2023-08-14 10:53:13 +0800

    add bandwidth

--- p301sta.py
+++ p301sta.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 
 import os
-import updateonce
-import json
-import sys
 
 import paramiko
-from scp import SCPClient
 from time import sleep
-import io
 
 
 def execcmd(remoteip, usr, password, cmds):
@@ -98,6 +93,7 @@ def get_freq_khz(val):
 
 fetchtab = [0xd0, 0xd4, 0xd8, 0xdc, 0xe0, 0xe4, 0xe8, 0xec, 0xf0, 0xf4, 0xf8]
 fetchtab += [0x154, 0x15c, 0x164, 0x16c, 0x174, 0x17c, 0x184, 0x18c]
+fetchtab += [0x04, 0x150]
 
 
 def print_freq(regdict):
@@ -116,6 +112,24 @@ def print_pa(regdict):
         print("usr {0} 5g = {1} 2g ={2}".format(usr, val >> 2, val & 3))
 
 
+def get_bw_from_val(val):
+    bw = {0: "1.25MHZ", 1: "2.5MHZ", 2: "5MHZ",
+          3: "10MHZ", 4: "20MHZ", 5: "40MHZ"}
+
+    return bw[val]
+
+
+def print_bandwidth(regdict):
+    val = regdict[0x04]["valint"] >> 24 & 0xff
+    val = val >> 4 & 0x07
+    print("tx slot0 bw = {}".format(get_bw_from_val(val)))
+
+    val = regdict[0x150]["valint"] >> 24 & 0xff
+    val = val & 0x07
+    print("rx slot0 bw = {}".format(get_bw_from_val(val)))
+
+
+
 if __name__ == "__main__":
     boardusr = 'root'
     boardpass = 'artosyn'
@@ -126,5 +140,7 @@ if __name__ == "__main__":
     print_freq(regdict)
 
     print_pa(regdict)
+
+    print_bandwidth(regdict)
 
     os.system("pause")
